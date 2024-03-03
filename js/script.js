a7137097a95d4575b80b4ce8b5d04d7b8add09e1
Sahil <sahsahil1998@gmail.com>
2024-03-02 22:07:53 -0500

    dark mode switch and text done

--- js/script.js
+++ js/script.js
@@ -2,24 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme');
 
+    // Apply the initial theme and update the icon and hover text
     if (currentTheme) {
         document.body.classList.add(currentTheme);
-        updateIcon(currentTheme);
+        updateIconAndHoverText(currentTheme);
     }
 
     themeToggle.addEventListener('click', () => {
         let theme = document.body.classList.toggle('night-mode') ? 'night-mode' : 'light-mode';
         localStorage.setItem('theme', theme);
-        updateIcon(theme);
+        updateIconAndHoverText(theme);
     });
 
-    function updateIcon(theme) {
+    function updateIconAndHoverText(theme) {
         if (theme === 'night-mode') {
             themeToggle.classList.remove('fa-sun');
             themeToggle.classList.add('fa-moon');
+            themeToggle.setAttribute('data-hover-text', 'Switch to light mode'); // Update hover text for night mode
         } else {
             themeToggle.classList.remove('fa-moon');
             themeToggle.classList.add('fa-sun');
+            themeToggle.setAttribute('data-hover-text', 'Switch to dark mode'); // Update hover text for light mode
         }
     }
 });
