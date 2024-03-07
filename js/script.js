ddb8115ce3d169e50d9da4494489840c3b041987
Sahil <sahsahil1998@gmail.com>
2024-03-06 22:59:21 -0500

    contact page and form submission

--- js/script.js
+++ js/script.js
@@ -25,4 +25,36 @@ document.addEventListener('DOMContentLoaded', () => {
             themeToggle.setAttribute('data-hover-text', 'Switch to dark mode');
         }
     }
+
+    // Check if the form exists on the current page before adding event listener
+    const contactForm = document.getElementById('myForm');
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(e) {
+            e.preventDefault(); // Prevent the default form submission
+
+            var formData = new FormData(this); // Grab the form data
+
+            fetch('https://formspree.io/f/mgegzoyd', { // Use your Formspree endpoint
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json' 
+                },
+            })
+            .then(response => {
+                if (response.ok) {
+                    document.getElementById('myForm').style.display = 'none'; 
+                    document.getElementById('formResult').style.display = 'block'; 
+                    document.getElementById('formResult').innerHTML = '<p>Thank you for your message!</p>';
+                } else {
+                    document.getElementById('formResult').style.display = 'block'; 
+                    document.getElementById('formResult').innerHTML = '<p>Oops! There was a problem with your submission. Please try again.</p>';
+                }
+            })
+            .catch(error => {
+                document.getElementById('formResult').style.display = 'block'; 
+                document.getElementById('formResult').innerHTML = '<p>Oops! There was a problem with your submission. Please try again.</p>';
+            });
+        });
+    }
 });
